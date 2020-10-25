30c58207d7d175017f69a8e0307fdc368608fcd4
SWolfeZB <s.wolfe0999@gmail.com>
2020-10-25 18:01:34 -0400

    Lab 7

--- gallery.php
+++ gallery.php
@@ -1,4 +1,5 @@
 <?php
+require 'includes/dbhandler.php';
 require 'includes/header.php';
 ?>
 
@@ -7,7 +8,6 @@ require 'includes/header.php';
     <h1>Gallery</h1>
         <div class = "gallery-container">
             <?php
-            include_once 'includes/dbhandler.php';
             $sql = "SELECT * FROM product ORDER BY upload_date DESC";
             $stmt = mysqli_stmt_init($conn);
 
@@ -18,8 +18,8 @@ require 'includes/header.php';
                 $result = mysqli_stmt_get_result($stmt);
                 while($row = mysqli_fetch_assoc($result))   {
                     echo '<div class = "card">
-                        <a href= "#">
-                            <img src = "reviews/'.$row["picpath"].'">
+                        <a href="review.php?id='.$row['pid'].'">
+                            <img src="reviews/'.$row["picpath"].'">
                             <h3>'.$row["title"].'</h3>
                             <p>'.$row["descript"].'</p>
                         </a>
